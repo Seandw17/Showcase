6813bc48fc10eb596d9dce2eeb14ec11f30690ef
SeanDW17 <seanw171@hotmail.co.uk>
2020-05-20 15:13:03 +0100

    Added_Interactable_object

---
+++ Showcase/Assets/Scripts/InteractableObjectBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObjectBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        //TODO - Add code for whatever you want the user to do once they click on an object
+    }
+}

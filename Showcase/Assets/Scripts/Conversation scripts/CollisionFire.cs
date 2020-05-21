8117e0ba76eb711e8142b4f7baf3693611fc860c
Alec Lauder <alauder98@gmail.com>
2020-05-21 10:12:07 +0100

    Added Conversation system 1.0
    Will now branch out to avoid any conflicts

---
+++ Showcase/Assets/Scripts/Conversation scripts/CollisionFire.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+// Class to allow for a collision box to be fired on a mouse down
+public class CollisionFire : MonoBehaviour
+{
+    /// <summary>
+    /// Collider to turn on / off on mouse click
+    /// </summary>
+    [SerializeField] BoxCollider m_collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_collider.enabled = false;
+    }
+
+    private void Update()
+    {
+        // enable collider if mouse is down
+        m_collider.enabled = Input.GetMouseButtonDown(0);
+    }
+}
---
+++ Showcase/Assets/Scripts/Conversation scripts/ConversationStructs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+// struct to hold an individual response
+public struct s_response
+{
+    public string response;
+    public e_connotes feel;
+    public e_unlockFlag unlockCriteria;
+}
+
+// struct to hold a question
+public struct s_questionData
+{
+    public string question;
+    public List<s_response> options;
+}

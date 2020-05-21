c22104f70c8f94b0b40ac67ff1e07cab3c742d6a
Alec Lauder <alauder98@gmail.com>
2020-05-21 09:45:07 +0100

    Added Test Scripts for Interview

---
+++ Showcase/Assets/Scripts/ConversationScripts/ConversationEnums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+[Flags]
+public enum e_connotes
+{
+    CONFIDENCE = 1 << 0,
+    NERVOUSNESS = 1 << 1,
+    SKILL = 1 << 2,
+    LACKKNOWLEDGE = 1 << 3,
+    HAVEKNOWLEDGE = 1 << 4,
+    UNSURE = 1 << 5,
+    BOASTFUL = 1 << 6
+}
+
+public enum e_identifier
+{
+    START = 1,
+    GOOD = 2,
+    END = 3
+}
+
+public enum e_unlockFlag
+{
+    NONE = 0,
+    LAPTOP = 1,
+    BOOK = 2,
+    FLYER = 3,
+    PHONE = 4
+}

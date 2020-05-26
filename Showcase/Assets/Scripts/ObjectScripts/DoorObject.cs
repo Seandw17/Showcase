d881f6e6cc50632a100b6c8a4f436e3e24066aef
SeanDW17 <seanw171@hotmail.co.uk>
2020-05-26 16:11:15 +0100

    Added_More_Objects
    Converted ig_interactable to List instead of Array

---
+++ Showcase/Assets/Scripts/ObjectScripts/DoorObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorObject : InteractableObjectBase
+{
+    bool m_dooropen = false;
+    float m_rotationspeed = 1.0f;
+    Quaternion m_opendoorrotation;
+    Quaternion m_closedoorrotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        m_opendoorrotation = Quaternion.AngleAxis(90.0f, transform.up);
+        m_closedoorrotation = Quaternion.AngleAxis(0.0f, transform.up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_dooropen == true)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation,m_closedoorrotation , m_rotationspeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation,m_opendoorrotation , m_rotationspeed * Time.deltaTime);
+        }
+    }
+
+    public override void Interact()
+    {
+        m_dooropen = !m_dooropen;
+        
+    }
+}

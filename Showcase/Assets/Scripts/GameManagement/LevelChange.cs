30f994887287a48323473f769a0f7d3bfe9cce1b
Alec Lauder <alauder98@gmail.com>
2020-05-26 09:51:01 +0100

    Changed Name of scene manager file
    Was causing errors when using UnityEngine.SceneManager

---
+++ Showcase/Assets/Scripts/GameManagement/LevelChange.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    /// <summary>
+    /// Sets the current active scene to the 
+    /// </summary>
+    /// <param name="scene"> Scene we are changing to </param>
+    /// <param name="mode"> the change most we are using</param>
+    void OnSceneLoaded(Scene _scene, LoadSceneMode _mode)
+    {
+        Debug.Assert(_mode == LoadSceneMode.Additive,
+            "All scene changes should be done using LoadSceneMode.Additive" +
+            " failing to do so will delete the _preload scene");
+        SceneManager.SetActiveScene(_scene);
+        Debug.Log("Changing to scene: " + _scene.name);
+    }
+}

fbbf7ddc95c8ac376f9eaf92a54fd8f7d87dda0c
Alec Lauder <alauder98@gmail.com>
2020-06-15 15:05:59 +0100

    Begun working on title screen lerping stuff

---
+++ Showcase/Assets/Scripts/MainMenu/TitleScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TitleScreen : MonoBehaviour
+{
+    [SerializeField] KeyCode m_startKey;
+
+    [SerializeField] TextMeshPro _Starttext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Starttext.SetText("Press " + m_startKey.ToString() + "To start");
+
+        //TODO load in positions
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO LERP
+
+        // TODO ONCE HERE GO TO THE NEXT
+
+        // TODO ONCE DONE FOR ALL GIVEN AREAS, GO TO NEXT
+    }
+}

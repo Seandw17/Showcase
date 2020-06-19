ccaab5ccbd0bd46afbd90c269cddd7bb0afbbd75
Alec Lauder <alauder98@gmail.com>
2020-06-19 11:24:34 +0100

    Switched the Title screen to cinemachine

---
+++ Showcase/Assets/Scripts/TitleScreen/BackgroundFade.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using static FadeIn;
+
+public class BackgroundFade : MonoBehaviour
+{
+    Image m_image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_image = GetComponent<Image>();
+    }
+
+    public void Display()
+    {
+        StartCoroutine(FadeAsset(m_image, 0.5f, true));
+    }
+
+    public void Remove()
+    {
+        StartCoroutine(FadeAsset(m_image, 0.5f, false));
+    }
+}

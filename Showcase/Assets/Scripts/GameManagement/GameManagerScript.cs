2692222bdf6abe5b771d916ec21eee8b0f2a965f
SeanDW17 <seanw171@hotmail.co.uk>
2020-06-29 15:03:37 +0100

    Base Objective UI
    UI has the objective on and can be changed to the next objective using IncrementObjectIndex();

--- Showcase/Assets/Scripts/GameManagement/GameManagerScript.cs
+++ Showcase/Assets/Scripts/GameManagement/GameManagerScript.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour
 {
     //All of the HUDS should be added here so that they can be accessed in the editor
-    [SerializeField]
     public GameObject ig_PlayerPanel, ig_WardrobePanel, ig_LaptopPanel, ig_magazineWaitingRoom, ig_outfitExitWarning, ig_noResearchExitWarning;
+
+    [SerializeField]
+    Text m_objectivetext;
 
     //This is the HUD that is displayed to the screen at all times
     GameObject ig_currenthud;
 
-    
+    //Array of strings for the objective text
+    [SerializeField]
+    string[] m_objectivetextarray;
+
+    int m_objectiveindex;
+
     public CursorController m_cmScript;
 
     // Start is called before the first frame update
@@ -20,6 +28,10 @@ public class GameManagerScript : MonoBehaviour
         //SetCurrentHUD(ig_PlayerPanel);
        
         m_cmScript = GetComponent<CursorController>();
+
+      
+        DisplayObjectiveText();
+        
     }
 
     // Update is called once per frame
@@ -41,6 +53,15 @@ public class GameManagerScript : MonoBehaviour
         //Display the new HUD to the screen
         ig_currenthud.SetActive(true);
     }
-   
 
+    void DisplayObjectiveText()
+    {
+        m_objectivetext.text = m_objectivetextarray[m_objectiveindex];
+    }
+
+    public void IncrementObjectiveIndex()
+    {
+        m_objectiveindex++;
+        DisplayObjectiveText();
+    }
 }

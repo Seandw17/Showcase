62efc834ae3893e5b32b2c60a29f5f6d6cb2e896
TheOverlord213 <55699665+TheOverlord213@users.noreply.github.com>
2020-06-30 13:10:38 +0100

    Added floating text above the staff member

--- Showcase/Assets/Scripts/CafeScripts/StaffMemberObject.cs
+++ Showcase/Assets/Scripts/CafeScripts/StaffMemberObject.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
@@ -8,12 +10,15 @@ public class StaffMemberObject : InteractableObjectBase
 {
     public UnityEngine.AI.NavMeshAgent ig_staffAgent { get; private set; }
 
+    bool m_isInteractable = true;
     bool m_staffMemberTriggered = false;
     bool m_staffMemberMoving = false;
+    bool m_reachedDoor = false;
 
     public Transform[] ig_targetPoints; // the patrol points for the AI
+    public GameObject ig_staffMemberText;
+   
     private int m_destPoint = 0;
-
     private float m_currentWaitTime = 0.0f;
 
     // Start is called before the first frame update
@@ -36,7 +41,7 @@ public class StaffMemberObject : InteractableObjectBase
             if (seconds == 5)
             {
                 m_staffMemberMoving = true;
-                Debug.Log("Staff member begun pathfinding");
+                ig_staffMemberText.SetActive(false);
             }
         }
         else if (m_staffMemberMoving)
@@ -46,34 +51,46 @@ public class StaffMemberObject : InteractableObjectBase
         
     }
 
+
     public override void Interact()
     {
-        Debug.Log("Staff member triggered");
-        m_staffMemberTriggered = true;
+        if (m_isInteractable)
+        {
+            Debug.Log("Staff member triggered");
+            m_isInteractable = false;
+            m_staffMemberTriggered = true;
+            ig_staffMemberText.GetComponentInChildren<TextMeshPro>().text = "Ah you're here for the interview. This way please.";
+        }
     }
 
+    // starts the pathfinding towards the door
     void AIPathfinding()
     {
-        if (ig_targetPoints.Length == 0)
-            return;
-        else
+        if (!m_reachedDoor)
         {
-            ig_staffAgent.SetDestination(ig_targetPoints[m_destPoint].position);
-        }
-
-
-        if (!ig_staffAgent.pathPending && ig_staffAgent.remainingDistance < 0.5f)
-        {
-            m_destPoint += 1;
-            if (m_destPoint > ig_targetPoints.Length)
+            if (ig_targetPoints.Length == 0)
+                return;
+            else
             {
-                Debug.Log("Staff member reached door");
-                m_staffMemberMoving = false;
-                m_staffMemberTriggered = false;
+                if (m_destPoint != ig_targetPoints.Length)
+                    ig_staffAgent.SetDestination(ig_targetPoints[m_destPoint].position);
             }
         }
 
-
-        
+        if (!ig_staffAgent.pathPending && ig_staffAgent.remainingDistance < 0.5f)
+        {
+            if (m_destPoint != ig_targetPoints.Length)
+            {
+                m_destPoint += 1;
+            }
+            else if (m_destPoint == ig_targetPoints.Length)
+            {
+                m_reachedDoor = true;
+                m_staffMemberMoving = false;
+                m_staffMemberTriggered = false;
+                ig_staffMemberText.SetActive(true);
+                ig_staffMemberText.GetComponentInChildren<TextMeshPro>().text = "Please enter through here.";
+            }
+        }        
     }
 }

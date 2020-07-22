b4dfa0ddb2fee85f18d4ee87b38ffb2be4db1927
Alec Lauder <alauder98@gmail.com>
2020-07-22 12:19:26 +0100

    Begun Adding Interview Unit tests to project

---
+++ Showcase/Assets/Tests/InterviewTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class InterviewTest
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestInterviewSetUp()
+        {
+            Object.Instantiate(
+                Resources.Load<GameObject>("Prefabs/QuestionSpace"));
+
+            yield return null;
+        }
+    }
+}
